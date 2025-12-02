2456c442a2e861271171bba20d74424e3f06effb
zzoop69 <24028856@myrp.edu.sg>
2025-12-02 12:37:31 +0800

    Initial commit: Added all project files and set up the full project structure.

--- SupermarketAppMVC1.0/models/Order.js
+++
@@ -1,4 +0,0 @@
-// Order model removed
-// The confirm-purchase/order creation functionality was intentionally disabled per user request.
-// If later re-enabled, implement transaction-safe order creation here.
-module.exports = {};
